d8c97849feee3fbc67dc21f1489776d565c9238c
Zac Palin <zpalin@gmail.com>
2015-10-25 13:36:33 -0700

    Filtered contracts search

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -6,7 +6,7 @@ class Contract < ActiveRecord::Base
     Unspsc.all.each do |unspsc|
       count = recs.where("full_code LIKE '" + unspsc.number + "%'").count
       if count > 0
-        results << unspsc.contract
+        results << unspsc.contract if unspsc.contract.epp_compliant?
       end
     end
     return results
